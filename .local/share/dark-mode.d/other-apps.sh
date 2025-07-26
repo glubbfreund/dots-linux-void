abd30d856d498cf53228fc56c627a63f40b7eb9f
Glubbfreund <oliver.leis@gmail.com>
2025-07-26 11:47:51 +0200

    added darkman scripts

---
+++ .local/share/dark-mode.d/other-apps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/.config/OpenRGB/OpenRGB.json.dark ~/.config/OpenRGB/OpenRGB.json
+sudo /home/oli/Dokumente/Src/spotifyIconPatch/install-spotify-dark.sh
---
+++ .local/share/light-mode.d/other-apps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/.config/OpenRGB/OpenRGB.json.light ~/.config/OpenRGB/OpenRGB.json
+sudo /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/install-spotify-light.sh
