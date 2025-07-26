816f7a2835c25c8c55f9fc09ea2f41ecbd0fa14d
Glubbfreund <oliver.leis@gmail.com>
2025-07-26 11:38:43 +0200

    updated iconpatch

---
+++ Dokumente/Src/spotifyIconPatch/icons_light/install-spotify-light.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# ~/Dokumente/Src/spotifyIcons/icons_light/install-spotify-light.sh
+
+cp /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/*.png /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/*.svg /usr/share/spotify/icons/
---
+++ Dokumente/Src/spotifyIconPatch/install-spotify-dark.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# ~/Dokumente/Src/spotifyIcons/install-spotify-dark.sh
+
+cp /home/oli/Dokumente/Src/spotifyIconPatch/*.png /home/oli/Dokumente/Src/spotifyIconPatch/*.svg /usr/share/spotify/icons/
