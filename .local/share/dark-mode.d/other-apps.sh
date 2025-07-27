6b3b160486b8451ba13ee707c7550e5515571c59
Glubbfreund <oliver.leis@gmail.com>
2025-07-27 17:11:50 +0200

    made patch for steam tray icon

--- .local/share/dark-mode.d/other-apps.sh
+++ .local/share/dark-mode.d/other-apps.sh
@@ -2,3 +2,4 @@
 
 cp ~/.config/OpenRGB/OpenRGB.json.dark ~/.config/OpenRGB/OpenRGB.json
 sudo /home/oli/Dokumente/Src/spotifyIconPatch/install-spotify-dark.sh
+cp /home/oli/Dokumente/Src/steamIconPatch/dark.png /home/oli/.local/share/Steam/public/steam_tray_mono.png
--- .local/share/light-mode.d/other-apps.sh
+++ .local/share/light-mode.d/other-apps.sh
@@ -2,3 +2,4 @@
 
 cp ~/.config/OpenRGB/OpenRGB.json.light ~/.config/OpenRGB/OpenRGB.json
 sudo /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/install-spotify-light.sh
+cp /home/oli/Dokumente/Src/steamIconPatch/light.png /home/oli/.local/share/Steam/public/steam_tray_mono.png
