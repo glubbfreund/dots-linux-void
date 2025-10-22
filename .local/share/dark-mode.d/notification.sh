3704ebb05809b7f20ee263690ea5949ffaed196b
Glubbfreund <oliver.leis@gmail.com>
2025-10-22 11:31:49 +0200

    dont run darkman notification after fresh sys start

--- .local/share/dark-mode.d/notification.sh
+++ .local/share/dark-mode.d/notification.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Do not run on system start
+uptime_seconds=$(awk '{print int($1)}' /proc/uptime)
+if [ "$uptime_seconds" -lt 30 ]; then
+    exit 0
+fi
+
 notify-send --app-name="darkman" --urgency=low --icon=weather-clear-night "Darmkan" "Zu Nachtmodus gewechselt."
--- .local/share/light-mode.d/notification.sh
+++ .local/share/light-mode.d/notification.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Nicht ausführen, wenn System kürzer als 2 Minuten läuft
+uptime_seconds=$(awk '{print int($1)}' /proc/uptime)
+if [ "$uptime_seconds" -lt 30 ]; then
+    exit 0
+fi
+
 notify-send --app-name="darkman" --urgency=low --icon=weather-clear "Darkman" "Zu hellem Modus gewechselt."
