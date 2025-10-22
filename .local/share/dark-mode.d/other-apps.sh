b21de718d9e2eb4764db1f6f4d26c131452482bb
Glubbfreund <oliver.leis@gmail.com>
2025-10-22 10:46:39 +0200

    added printer driver setup;other apps now include prismlauncher

--- .local/share/dark-mode.d/other-apps.sh
+++ .local/share/dark-mode.d/other-apps.sh
@@ -3,3 +3,4 @@
 cp ~/.config/OpenRGB/OpenRGB.json.dark ~/.config/OpenRGB/OpenRGB.json
 sudo /home/oli/Dokumente/Src/spotifyIconPatch/install-spotify-dark.sh
 cp /home/oli/Dokumente/Src/steamIconPatch/dark.png /home/oli/.local/share/Steam/public/steam_tray_mono.png
+cp /home/oli/.local/share/PrismLauncher/prismlauncher-dark.cfg /home/oli/.local/share/PrismLauncher/prismlauncher.cfg
--- .local/share/light-mode.d/other-apps.sh
+++ .local/share/light-mode.d/other-apps.sh
@@ -3,3 +3,4 @@
 cp ~/.config/OpenRGB/OpenRGB.json.light ~/.config/OpenRGB/OpenRGB.json
 sudo /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/install-spotify-light.sh
 cp /home/oli/Dokumente/Src/steamIconPatch/light.png /home/oli/.local/share/Steam/public/steam_tray_mono.png
+cp /home/oli/.local/share/PrismLauncher/prismlauncher-light.cfg /home/oli/.local/share/PrismLauncher/prismlauncher.cfg
