c3cc25bb9fc6d351dea06489c536df4d43a0beeb
Glubbfreund <oliver.leis@gmail.com>
2025-10-25 10:20:08 +0200

    fixed missing folder in path

--- Dokumente/Src/customIcons/spotifyIconPatch/install-spotify-dark.sh
+++ Dokumente/Src/customIcons/spotifyIconPatch/install-spotify-dark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # ~/Dokumente/Src/spotifyIcons/install-spotify-dark.sh
 
-cp /home/oli/Dokumente/Src/spotifyIconPatch/*.png /home/oli/Dokumente/Src/spotifyIconPatch/*.svg /usr/share/spotify/icons/
+cp /home/oli/Dokumente/Src/customIcons/spotifyIconPatch/*.png /home/oli/Dokumente/Src/customIcons/spotifyIconPatch/*.svg /usr/share/spotify/icons/
